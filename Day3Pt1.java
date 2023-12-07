bf30679ed974cfe45efec14caefc3efd5714b65a
kxinhao <kxinhao@gmail.com>
2023-12-07 17:21:34 +0800

    reversed coordinates of Points due to nature of 2d array creation

--- Day3Pt1.java
+++ Day3Pt1.java
@@ -23,16 +23,19 @@ public class Day3 {
             for(int i = 0; i < schematic2D.length; i++) {
                 Point wordStart = null;
                 Point wordEnd = null;
+                String lineNo = "";
                 for(int j = 0; j < schematic[i].length; j++) {
-                    char schematicChar = schematic[i][j];
+                    char schematicChar = schematic[j][i];
                     if(Character.digit(schematicChar,10)>=0) {
                         if(lineNo.isEmpty()) {
-                           wordStart = new Point(i,j); 
+                           wordStart = new Point(j,i); 
                         }
                         lineNo = lineNo + schematicChar;
                     } else if(!lineNo.isEmpty()){
+                        wordEnd = new Point(j-1,i-1);
+                        if(
                         validPartNoSum += Integer.parseInt(lineNo);
-                        wordEnd = new Point(i,j);
+                        lineNo = "";
                     }
                 }
             }
@@ -44,5 +47,27 @@ public class Day3 {
         }
     }
 
-    public boolean
+    public boolean checkEdges(Point startPoint, Point endPoint, Character[][] schematic) {
+
+        // accounting for single digit numbers
+        int startX = (int)startPoint.getX();
+        int startY = (int)startPoint.getY();
+        int endX = (int)endPoint.getX();
+        int endY = (int)endPoint.getY();
+
+        if(startY > 0 && endY < schematic.length-1 && startX > 0 && endX < schematic[0].length-1) {
+            
+        }
+
+        if(startY <= 0 || endY <= 0) {
+
+        } else if(startY >= schematic.length-1 || endY >= schematic.length-1) {
+
+        } else if(startX <= 0 || endX <= 0) {
+
+        } else if(startX >= schematic[0].length-1 || endX >= schematic[0].length-1) {
+
+        }
+
+    }
 }
