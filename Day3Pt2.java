c22bfdb56277ed272efdfb8f4af9a278478bd5f2
kxinhao <kxinhao@gmail.com>
2023-12-16 04:47:22 +0800

    added outline for day3pt2 solution

---
+++ Day3Pt2.java
@@ -0,0 +1,124 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.awt.Point;
+import java.util.regex.*;
+import java.util.ArrayList;
+
+public class Day3 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader("day3_input.txt")); 
+        FileWriter fw = new FileWriter("Day3Pt2_Output.txt");
+        int validPartNoSum = 0;
+        ArrayList<char[]> schematic = new ArrayList<char[]>(); 
+        String line = null;
+
+        try {
+            while((line = br.readLine()) != null) {
+                char[] row = new char[line.length()]; 
+                for(int i = 0; i < line.length(); i++) {
+                    row[i] = line.charAt(i);
+                }
+                schematic.add(row);
+            }
+            char[][] schematic2D = new char[schematic.size()][];
+            schematic2D = schematic.toArray(schematic2D);
+            for(int i = 0; i < schematic2D.length; i++) {
+                fw.write("\n ********** processing line " + (i+1));
+                Point wordStart = null;
+                Point wordEnd = null;
+                String lineNo = "";
+                for(int j = 0; j < schematic2D[i].length; j++) {
+                    char schematicChar = schematic2D[i][j];
+                    int xPoint = j - 1;
+                    int yPoint = i;
+                    //System.out.println("checking char: " + schematicChar);
+                    if(Character.digit(schematicChar,10)>=0) {
+                        if(lineNo.isEmpty()) {
+                           wordStart = new Point(j,i); 
+                        }
+                        lineNo = lineNo + schematicChar;
+                        // for nums with last digit at EOL
+                        if(j == schematic2D[i].length - 1) {
+                            xPoint = j;
+                            wordEnd = new Point(xPoint, yPoint);
+                            fw.write("\n !! completed eol number is: " + lineNo);
+                            if(isValidNum(wordStart, wordEnd, schematic2D)) {
+                                fw.write("\n lineNo: " + lineNo + " is valid, adding to sum: " + validPartNoSum);
+                                validPartNoSum += Integer.parseInt(lineNo);
+                            } else {
+                                fw.write("\n lineNo: " + lineNo + " is not valid");
+                            }
+                        lineNo = "";
+                        }
+                    } else if(!lineNo.isEmpty()){
+                        wordEnd = new Point(xPoint, yPoint);
+                        fw.write("\n !! completed number is: " + lineNo);
+                        if(isValidNum(wordStart, wordEnd, schematic2D)) {
+                            fw.write("\n lineNo: " + lineNo + " is valid, adding to sum: " + validPartNoSum);
+                            validPartNoSum += Integer.parseInt(lineNo);
+                        } else {
+                            fw.write("\n lineNo: " + lineNo + " is not valid");
+                        }
+                        lineNo = "";
+                    } 
+                }
+                fw.write("\n ###### end processing of line " + (i+1) + ", valid value is: " + validPartNoSum);
+            }
+            fw.write("\n Sum of schematic part numbers: "+validPartNoSum);
+        }catch(IOException e) {
+            System.out.println(e.getMessage());
+        }finally {
+            br.close();
+            fw.close();
+        }
+    }
+
+    public static boolean isValidNum(Point startPoint, Point endPoint, char[][] schematic) {
+
+        // accounting for single digit numbers
+        int startX = (int)startPoint.getX();
+        int startY = (int)startPoint.getY();
+        int endX = (int)endPoint.getX();
+        int endY = (int)endPoint.getY();
+        //System.out.println("start coords are: " + startX + ", " + startY);
+        //System.out.println("end coords are: " + endX + ", " + endY);
+        int bufferStartX = startX - 1;
+        int bufferStartY = startY - 1;
+        int bufferEndX = endX + 1;
+        int bufferEndY = endY + 1;
+
+        boolean isValidNum = false;
+
+        // accounting for edge cases
+        // top of file
+        if (startY <= 0 || endY <= 0) { 
+            bufferStartY = startY;
+        }
+        // bottom of file
+        if (startY >= schematic.length-1 || endY >= schematic.length-1) {
+            bufferEndY = endY;
+        }
+        // left
+        if (startX <= 0 || endX <= 0) {
+            bufferStartX = startX;
+        }
+        // right
+        if (startX >= schematic[0].length - 1 || endX >= schematic[0].length - 1) {
+            bufferEndX = endX;
+        }
+
+        Pattern pattern = Pattern.compile("[^0-9.\s]");
+
+        for (int y = bufferStartY; y <= bufferEndY; y++) {
+            for (int x = bufferStartX; x <= bufferEndX; x++) {
+                if(pattern.matcher(""+schematic[y][x]).find()) {
+                    isValidNum = true;
+                    return isValidNum;
+                }
+            }
+        }
+        return isValidNum;
+    }
+}
