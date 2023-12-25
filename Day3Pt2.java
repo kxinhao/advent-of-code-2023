1bfa7df8d42e20c88bc0dbb5ee5614dc5e8de457
kxinhao <kxinhao@gmail.com>
2023-12-26 02:14:46 +0800

    fixed logic for buffer creation and start/end of line checks

--- Day3Pt2.java
+++ Day3Pt2.java
@@ -16,10 +16,10 @@ public class Day3Pt2 {
     private static FileHandler fh;
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new FileReader("day3_input_ex.txt")); 
+        BufferedReader br = new BufferedReader(new FileReader("day3_input_ex.txt"));
         int gearRatioSum = 0;
-        ArrayList<char[]> schematic = new ArrayList<char[]>(); 
-        Map<Integer,Point> gearMap = new HashMap<Integer,Point>();
+        ArrayList<char[]> schematic = new ArrayList<char[]>();
+        Map<Integer, Point> gearMap = new HashMap<Integer, Point>();
         String line = null;
         int lineInd = 0;
         int gearCount = 0;
@@ -29,10 +29,10 @@ public class Day3Pt2 {
             LOG.addHandler(fh);
             fh.setFormatter(new SimpleFormatter());
             Pattern pattern = Pattern.compile("[*]");
-            while((line = br.readLine()) != null) {
-                char[] row = new char[line.length()]; 
-                for(int i = 0; i < line.length(); i++) {
-                    if(pattern.matcher(""+line.charAt(i)).find()) {
+            while ((line = br.readLine()) != null) {
+                char[] row = new char[line.length()];
+                for (int i = 0; i < line.length(); i++) {
+                    if (pattern.matcher("" + line.charAt(i)).find()) {
                         gearMap.put(Integer.valueOf(gearCount), new Point(i, lineInd));
                         gearCount++;
                     }
@@ -44,20 +44,20 @@ public class Day3Pt2 {
             char[][] schematic2D = new char[schematic.size()][];
             schematic2D = schematic.toArray(schematic2D);
             // check each gear for adjacent numbers
-            for(int i = 0; i < gearMap.size(); i++) {
+            for (int i = 0; i < gearMap.size(); i++) {
                 Point gearCoords = gearMap.get(i);
-                int gearX = (int)gearCoords.getX();
-                int gearY = (int)gearCoords.getY();
-                LOG.info("\n checking gear "+i+" in line "+gearY);
+                int gearX = (int) gearCoords.getX();
+                int gearY = (int) gearCoords.getY();
+                LOG.info("\n checking gear " + i + " in line " + gearY);
                 int gearRatio = calculateGearRatio(gearX, gearY, schematic2D);
                 gearRatioSum += gearRatio;
                 LOG.info("Current gear ratio sum: " + gearRatioSum);
             }
 
-        LOG.info("\n Sum of gear ratios: "+gearRatioSum);
-        }catch(IOException e) {
+            LOG.info("\n Sum of gear ratios: " + gearRatioSum);
+        } catch (IOException e) {
             LOG.info(e.getMessage());
-        }finally {
+        } finally {
             br.close();
         }
     }
@@ -70,14 +70,14 @@ public class Day3Pt2 {
         int bufferEndX = gearX + 1;
         int bufferEndY = gearY + 1;
 
-        LOG.info("X: " + gearX + " , Y:" + gearY); 
+        LOG.info("X: " + gearX + " , Y:" + gearY);
         // accounting for edge cases
         // top of file
-        if (gearY <= 0) { 
+        if (gearY <= 0) {
             bufferStartY = gearY;
         }
         // bottom of file
-        if (gearY >= schematic.length-1) {
+        if (gearY >= schematic.length - 1) {
             bufferEndY = gearY;
         }
         // left
@@ -85,35 +85,34 @@ public class Day3Pt2 {
             bufferStartX = gearX;
         }
         // right
-        if (gearX >= schematic[0].length - 4) {
+        if (gearX >= schematic[0].length - 1) {
             bufferEndX = gearX;
         }
         LOG.info("bufferStartY: " + bufferStartY + " , bufferEndY: " + bufferEndY);
         LOG.info("bufferStartX: " + bufferStartX + " , bufferEndX: " + bufferEndX);
 
         Pattern digit = Pattern.compile("[0-9]");
- /*       
-        String numOne = "";
-        String numTwo = "";
-        boolean numOneDone = false;
-        boolean numTwoDone = false;
-        */
+        /*
+         * String numOne = "";
+         * String numTwo = "";
+         * boolean numOneDone = false;
+         * boolean numTwoDone = false;
+         */
         String number = "";
-        int startInd = 0, endInd = 0;
         List<Integer> adjacentNumList = new ArrayList<Integer>();
 
         for (int y = bufferStartY; y <= bufferEndY; y++) {
             for (int x = bufferStartX; x <= bufferEndX; x++) {
-                if(digit.matcher(""+schematic[y][x]).find()) {
+                if (digit.matcher("" + schematic[y][x]).find()) {
                     // adjacent num found, complete num
-                    number = findNum(x, schematic[y]); 
+                    number = findNum(x, schematic[y]);
                     adjacentNumList.add(Integer.valueOf(number));
                     x = x + number.length();
                 }
             }
         }
 
-        if(adjacentNumList.size() == 2) {
+        if (adjacentNumList.size() == 2) {
             gearRatio = adjacentNumList.get(0) * adjacentNumList.get(1);
         }
         LOG.info("gearRatio: " + gearRatio);
@@ -125,14 +124,15 @@ public class Day3Pt2 {
         StringBuffer sb = new StringBuffer();
         int xPos = x;
         Pattern endPattern = Pattern.compile("[^0-9]");
-        while(!endPattern.matcher(""+schematicLine[xPos]).find() && xPos>0) {
+        while (xPos > 0 && !endPattern.matcher("" + schematicLine[xPos]).find()) {
             xPos--;
         }
-        if(endPattern.matcher(""+schematicLine[xPos]).find()) {
+        if (endPattern.matcher("" + schematicLine[xPos]).find()) {
             xPos++;
         }
-        while(!endPattern.matcher(""+schematicLine[xPos]).find() && xPos<schematicLine.length) {
+        while (xPos < schematicLine.length && !endPattern.matcher("" + schematicLine[xPos]).find()) {
             sb.append(schematicLine[xPos]);
+            LOG.info("adding digit: "+schematicLine[xPos]);
             xPos++;
         }
         LOG.info("Num found is: " + sb.toString());
